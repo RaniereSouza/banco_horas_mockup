1b49fc41d1a51de859affc05294d4e7bb7da5af0
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2020-03-16 20:36:27 -0300

    criando métodos fake de login e logout

--- banco_horas_mockup/Controllers/LoginController.cs
+++ banco_horas_mockup/Controllers/LoginController.cs
@@ -11,9 +11,34 @@ namespace banco_horas_mockup.Controllers
         // GET: /Login
         public ActionResult Index()
         {
-            ViewBag.Message = "Faça seu login:";
-
             return View();
+        }
+
+        // POST: /Login/HandleLogin
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult HandleLogin (string login, string senha)
+        {
+            if (login == "RaniereSouza" && senha == "123456")
+            {
+                Session["loggedIn"] = true;
+                Session["username"] = login;
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.ErrorMessage = "Login ou senha incorretos";
+
+            return View("Index");
+        }
+
+        // GET: /Login/HandleLogout
+        public ActionResult HandleLogout()
+        {
+            Session["loggedIn"] = false;
+            Session["username"] = "";
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
